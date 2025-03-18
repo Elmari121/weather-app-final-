1d84a73297bf05a038547bef4435d0ef398307bd
Elmari121 <robbertselmari@gmail.com>
2025-03-18 11:11:01 +0200

    js update

--- script.js
+++ script.js
@@ -2,67 +2,64 @@ function getweather() {
   let apikey = "30927dtfa44b4770359oe8258a9c5b2c";
   let city = document.getElementById("city").value;
   if (!city) {
-    alert("please enter a city name");
+    alert("Please enter a city name.");
     return;
   }
-}
-let currentweatherurl =
-  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}";
-let forecasturl =
-  "https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}";
-function getweather() {
+
+  let currentweatherurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}`;
+  let forecasturl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apikey}`;
+
   fetch(currentweatherurl)
-    .then((Response) => Response.json())
+    .then((response) => response.json())
     .then((data) => {
       displayweather(data);
     })
     .catch((error) => {
-      console.error("error fetching current weather data ", error);
+      console.error("Error fetching current weather data: ", error);
     });
-}
-function getForecast() {
+
   fetch(forecasturl)
-    .then((Response) => Response.json())
+    .then((response) => response.json())
     .then((data) => {
-      displayforecast(data.list);
+      displayhourlyforecast(data.forecast);
     })
     .catch((error) => {
-      console.error("error fetching forecast data ", error);
+      console.error("Error fetching forecast data: ", error);
     });
 }
+
 function displayweather(data) {
   let tempdivinfo = document.getElementById("temp-div");
   let weatherinfodiv = document.getElementById("weather-info");
   let weathericon = document.getElementById("weather-icon");
-  let hourlyforecastdiv = document.getElementById("hourly-forecast");
+  let hourlyforecastdiv = document.getElementById("hourly-forcast");
 
   weatherinfodiv.innerHTML = "";
   hourlyforecastdiv.innerHTML = "";
   tempdivinfo.innerHTML = "";
 
   if (data.cod === "404") {
-    weatherinfodiv.innerHTML = "<p>${data.message}</p>";
+    weatherinfodiv.innerHTML = `<p>${data.message}</p>`;
   } else {
     let cityname = data.name;
     let temperature = Math.round(data.main.temp - 273.15);
     let description = data.weather[0].description;
     let iconcode = data.weather[0].icon;
-    let iconurl = "";
+    let iconurl = `http://openweathermap.org/img/wn/${iconcode}.png`;
+
+    let temperaturehtml = `<p>${temperature}°C</p>`;
+    let weatherhtml = `<p>${cityname}</p><p>${description}</p>`;
+
+    tempdivinfo.innerHTML = temperaturehtml;
+    weatherinfodiv.innerHTML = weatherhtml;
+    weathericon.src = iconurl;
+    weathericon.alt = description;
+    weathericon.style.display = "block";
   }
 }
 
-let temperaturehtml = "<p>${temperature}°C</p>";
-let weatherhtml = "<p>${cityname}</p><p>${description}</p>";
-
-tempdivinfo.innerHTML = temperaturehtml;
-weatherinfodiv.innerHTML = weatherhtml;
-weathericon.src = iconurl;
-weathericon.alt = description;
-
-showimage();
-
 function displayhourlyforecast(hourlydata) {
-  let hourlyforecastdiv = document.getElementById("hourly-forecast");
+  let hourlyforecastdiv = document.getElementById("hourly-forcast");
   let next24hours = hourlydata.slice(0, 8);
 
   next24hours.forEach((item) => {
@@ -73,15 +70,11 @@ function displayhourlyforecast(hourlydata) {
     let iconurl = `http://openweathermap.org/img/wn/${iconcode}.png`;
 
     let hourlyitemhtml = `<div class='hourly-item'> 
-    <span>${hour}:00</span>
-    <img src='${iconurl}' alt='hourly weather icon'>
-    <span>${temperature}°C</span>
+      <span>${hour}:00</span>
+      <img src='${iconurl}' alt='hourly weather icon'>
+      <span>${temperature}°C</span>
     </div>`;
+
     hourlyforecastdiv.innerHTML += hourlyitemhtml;
   });
-}
-
-function showimage() {
-  let weathericon = document.getElementById("weather-icon");
-  weathericon.style.display = "block";
 }
