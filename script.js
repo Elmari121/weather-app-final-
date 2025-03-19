460a54614823aa4b7bad576cd07041213d33bcc4
Elmari121 <robbertselmari@gmail.com>
2025-03-19 12:10:48 +0200

    api key api url

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function searchCity(city) {
+  let apiKey = "30927dtfa44b4770359oe8258a9c5b2c";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric";
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
